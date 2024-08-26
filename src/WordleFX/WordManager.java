bdb30a8c0f26dbe7d6b328e5de4200d9633772bb
Khant Nyi <khantnyithu@gmail.com>
2024-08-27 02:32:16 +0700

    Create WordManager.java

---
+++ src/WordleFX/WordManager.java
@@ -0,0 +1,26 @@
+package application;
+
+public class WordManager {
+
+    private String secretWord;
+
+    public void setSecretWord(String secretWord) {
+        this.secretWord = secretWord.toUpperCase();
+    }
+
+    public boolean processGuess(String guess) {
+        return guess.equals(secretWord);
+    }
+
+    public char getSecretCharAt(int index) {
+        return secretWord.charAt(index);
+    }
+
+    public boolean containsChar(char letter) {
+        return secretWord.contains(String.valueOf(letter));
+    }
+
+    public String getSecretWord() {
+        return secretWord;
+    }
+}
